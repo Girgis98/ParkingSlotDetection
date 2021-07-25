b7718901d4f16488d71429688e569ae085aed800
Girgis98 <52557627+Girgis98@users.noreply.github.com>
2021-07-25 04:31:36 +0200

    Update training.py

--- Training/training.py
+++ Training/training.py
@@ -7,13 +7,33 @@ from Utils.process import complete_marking_vector_label_mult
 
 
 def model_training(model, myloss, optimizer, scheduler, num_epochs, data_loader, device, actual_batch_size = 1024):
+    '''
+    Model Training Function where weights and visualization statistics are saved to predefined paths
+    :param model: Model to be trained
+    :type model: nn.Module
+    :param myloss: Loss function
+    :type myloss: Function
+    :param optimizer: Training Optimizer
+    :type optimizer: optim
+    :param scheduler: Lr Scheduler
+    :type scheduler: scheduler
+    :param num_epochs: Number of Training Epochs
+    :type num_epochs: int
+    :param data_loader: Data Loader
+    :type data_loader: dataloader
+    :param device: Device to train on
+    :type device: torch device
+    :param actual_batch_size: Wanted Batch Size
+    :type actual_batch_size: int
+    :return: 0 on success
+    :rtype: int
+    '''
     graph_itrator = 0
     graph_itrator_2 = 0
     batch_loss = 0
     writer = SummaryWriter('save_path')  # for tensorboard visualization
     BS = data_loader.batch_size
     actual_batch_factor = actual_batch_size / BS
-    number_of_batches = math.ceil(len(data_loader.dataset.sample_names) / BS)
     model = model.train()
 
     for epoch in range(num_epochs):
