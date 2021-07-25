d6ae5e8976f8c892a9ecc389e785df859e851abc
CarooKaram <53713334+CarooKaram@users.noreply.github.com>
2021-07-25 02:00:44 +0200

    Add files via upload

---
+++ model.py
@@ -0,0 +1,167 @@
+import torch.nn as nn
+from Slot.slot_inference import predict, get_predicted_points, non_maximum_suppression
+#from model import MarkingPointDetector
+from visualizer import visualize_after_thres
+import torch
+import cv2
+from torchvision.transforms import ToTensor
+
+device = torch.device("cuda")
+
+
+# Convolutional neural network
+class MarkingPointDetector(nn.Module):
+
+    def __init__(self):
+        """
+        initialization of Marking Point Detector class
+        """
+        super(MarkingPointDetector, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size = 3, stride = 1, padding = 1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(32, 64, kernel_size = 4, stride = 2, padding = 1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(64, 32, kernel_size = 1, stride = 1, padding = 0),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(32, 64, kernel_size = 3, stride = 1, padding = 1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(64, 128, kernel_size = 4, stride = 2, padding = 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(128, 64, kernel_size = 1, stride = 1, padding = 0),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(64, 128, kernel_size = 3, stride = 1, padding = 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(128, 256, kernel_size = 4, stride = 2, padding = 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(256, 128, kernel_size = 1, stride = 1, padding = 0),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(128, 256, kernel_size = 3, stride = 1, padding = 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(256, 128, kernel_size = 1, stride = 1, padding = 0),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(128, 256, kernel_size = 3, stride = 1, padding = 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(256, 512, kernel_size = 4, stride = 2, padding = 1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(512, 256, kernel_size = 1, stride = 1, padding = 0),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(256, 512, kernel_size = 3, stride = 1, padding = 1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(512, 256, kernel_size = 1, stride = 1, padding = 0),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(256, 512, kernel_size = 3, stride = 1, padding = 1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(512, 1024, kernel_size = 4, stride = 2, padding = 1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(1024, 512, kernel_size = 1, stride = 1, padding = 0),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(512, 1024, kernel_size = 3, stride = 1, padding = 1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.1))
+
+        self.predict = nn.Sequential(
+            nn.Conv2d(1024, 512, kernel_size = 1, stride = 1, padding = 0),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(512, 1024, kernel_size = 3, stride = 1, padding = 1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(1024, 512, kernel_size = 1, stride = 1, padding = 0),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(512, 1024, kernel_size = 3, stride = 1, padding = 1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.1),
+
+            nn.Conv2d(1024, 6, kernel_size = 1, stride = 1, padding = 0)
+        )
+
+    def forward(self, x):
+        """
+        :param x: input value for CNN
+
+        :return: out: final output after passing through CNN
+        """
+        out = self.features(x)
+        out = self.predict(out)
+        out = out.reshape(out.size(0), -1)
+        return out
+
+
+"""
+### initialize marking points predictor
+"""
+
+
+def init_marking_points_model():
+    """
+    :return: model:  model after initialization with its weights
+    """
+    model = MarkingPointDetector().to(device)
+    checkpoint = torch.load(r'/model_weights')  # weights path
+    model.load_state_dict(checkpoint['model_state_dict'])
+    return model
+
+
+"""### Takes an image returns predicted Marking points"""
+
+
+def image_predict_marking_points(input_image, model):
+    """
+    :param input_image: input image to be passed for model for prediction
+    :param model: model used for prediction
+    :return: predict2: predicted points after applying confidence threshold and non max suppression
+    :return: out_image: output image from visualizer after drawing predicted points
+    """
+    image_transform = ToTensor()
+    input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
+    input_image = image_transform(input_image)
+
+    model = model.eval()
+    predict_awal = predict(input_image.reshape((1, 3, 512, 512)).to(device),model,device)
+    predict_ba3d = get_predicted_points(predict_awal, 70)
+    predict2 = non_maximum_suppression(predict_ba3d)
+    out_image = visualize_after_thres(input_image, predict2[0], False)
+    return predict2, out_image
