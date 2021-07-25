1b95c4103542d35ad4bb0a6fa9bcbed6bb1b56b7
CarooKaram <53713334+CarooKaram@users.noreply.github.com>
2021-07-25 03:19:46 +0200

    Add files via upload

--- slot_inference_main.py
+++ slot_inference_main.py
@@ -5,12 +5,13 @@ from Slot.slot_inference import inference_slots
 from model import init_marking_points_model, image_predict_marking_points
 from visualizer import visualize_slot
 
-device = torch.device("cuda")
+device = torch.device("cpu")
 model = init_marking_points_model()
 model = model.eval()
 
-image_path = r"image_path"
+image_path = r"par14.jpg"
 input_image = cv2.imread(image_path)
+
 input_image = cv2.medianBlur(input_image, 5)
 smoothed = cv2.GaussianBlur(input_image, (9, 9), 10)
 input_image = cv2.addWeighted(input_image, 2, smoothed, -1, 5)
